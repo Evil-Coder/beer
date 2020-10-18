2d342ad7628cb00697b1b2793f0d7ec8c9d3e7a9
Evil-Coder <roman123456777@gmail.com>
2020-10-19 00:01:18 +0300

    Завершил задание

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
   <div id="app">
 		<div class="element" >
 			<section class="element__container"
-							 v-for="beer of beers" :key="beer.id">
+							 v-for="(beer, i) of beers" :key="i">
 				<div class="element__head">
 					<h3 class="element__heading">{{beer.name}}</h3>
 					<p class="element__id">#{{beer.id}}</p>
 				</div>
 				<img class="element__img"
 						 @load="loadedState"
-						 :src="beer.image_url" alt="Beer image">
+						 :src="beer.image_url ? beer.image_url : '#' && loadedState()" alt="Beer image">
 				<div class="element__main-text">
 					<p class="element__tip">{{beer.brewers_tips}}</p>
 					<p class="element__description">{{beer.description}}</p>
-					<button class="element__button-edit">Edit</button>
-					<button class="element__button-delete">Delete</button>
+					<button class="element__button-edit"
+									@click.prevent="openEdit = !openEdit">Edit</button>
+					<button class="element__button-delete"
+									@click.prevent="deleteElem(i)">Delete</button>
+				</div>
+				<div v-if="openEdit" class="element__edit">
+					<div class="element__edit-name" contenteditable="true"
+							 @input="edit($event, 'heading', i)">{{beer.name}}</div>
+					<div class="element__edit-description" contenteditable="true"
+							 @input="edit($event, 'description', i)">{{beer.description}}</div>
 				</div>
 			</section>
 		</div>
@@ -25,19 +33,15 @@
 </template>
 
 <script>
-
-
 export default {
   name: 'App',
-  components: {
-
-  },
 	data() {
 		return {
 			page: 1,
 			beers: [],
 			loadedItems: 1,
-			loaded: false
+			loaded: false,
+			openEdit: false,
 		}
 	},
 	methods: {
@@ -46,10 +50,18 @@ export default {
 						.then(res => res.data.length === 0 ? this.page = false : this.beers.push(...res.data) && this.page++)
 		},
 		loadedState() {
+			if (this.loadedItems > this.beers.length) return
 			this.beers.length === this.loadedItems ? this.loaded = this.loadedItems++ : this.loaded = this.loadedItems++ && false
-		}
+		},
+		edit(ev, elem, id) {
+			const description = document.querySelectorAll(`.element__${elem}`)[id]
+			description.textContent = ev.target.innerText
+		},
+		deleteElem(i) {
+			this.beers.splice(i, 1)
+		},
 	},
-	  mounted() {
+	mounted() {
 		 this.fetchBeers()
 	},
 
@@ -62,11 +74,20 @@ export default {
 	display: grid
 	grid-template-columns: repeat(auto-fit, minmax(400px, 1fr))
 
+	@media (max-width: 400px)
+		display: flex
+		flex-direction: column
+
 	&__container
 		display: grid
+		position: relative
 		grid-template: 120px 1fr / 1fr 2fr
 		grid-gap: 20px
 		padding: 20px
+
+		@media (max-width: 400px)
+			display: flex
+			flex-direction: column
 
 	&__head
 		display: flex
@@ -75,6 +96,10 @@ export default {
 		justify-content: center
 		padding: 30px
 		background-color: #7418aa
+
+		@media (max-width: 400px)
+			width: 100%
+			box-sizing: border-box
 
 	&__heading
 		margin-right: 20px
@@ -88,6 +113,7 @@ export default {
 
 	&__img
 		width: 150px
+		margin: 0 auto
 
 	&__description
 		font-size: 20px
@@ -97,22 +123,47 @@ export default {
 
 	&__button
 		&-edit
-			margin-right: 10px
+			margin-right: 10%
 			width: 40%
-			height: 30px
+			height: 40px
 			border-radius: 30px
 			border: 1px solid white
 			background-color: #e9b814
 			font-size: 16px
 			font-weight: bold
+			outline: none
+
+			&:hover
+				cursor: pointer
+
 		&-delete
 			width: 40%
-			height: 30px
+			height: 40px
 			border-radius: 30px
 			border: 1px solid white
 			background-color: #db2d2d
 			font-size: 16px
 			font-weight: bold
+			outline: none
+
+			&:hover
+				cursor: pointer
+
+	&__edit
+		grid-column: 1 / -1
+		padding: 10px
+		border: 3px solid
+		background-color: white
+
+		&-name
+			margin-bottom: 20px
+			font-weight: bold
+			font-size: 20px
+			outline: none
+
+		&-description
+			font-size: 18px
+			outline: none
 
 .loadMore
 	display: block
@@ -122,5 +173,11 @@ export default {
 	margin: 20px auto
 	border-radius: 30px
 	background-color: #2fcbec
+	font-weight: bold
+	font-size: 20px
+	color: white
+
+	&:hover
+		cursor: pointer
 
 </style>
